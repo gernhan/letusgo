147ac23746fcb5d5fe7faae210e4b72d994e22a4
gernhan <gernhan@gmail.com>
2022-11-25 14:39:32 +0700

    finish udemy course

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,16 +15,29 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(100 * time.Millisecond)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		//c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
+	//c <- "Yep it's up"
+	c <- link
 }
