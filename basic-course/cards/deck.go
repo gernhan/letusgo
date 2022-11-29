a36e39f1d10534a8435f8e5a245120825f4a7f0b
gernhan <gernhan@gmail.com>
2022-11-29 21:56:12 +0700

    add folder basic-course

--- basic-course/cards/deck.go
+++ basic-course/cards/deck.go
@@ -22,8 +22,8 @@ func newDeckFromFile(fileName string) (deck, error) {
 
 func newDeck() deck {
 	cards := deck{}
-	cardSuits := deck{"Spades", "Diamonds", "Hearts"}
-	cardValues := deck{"Ace", "Two", "Three", "Four", "Five"}
+	cardSuits := deck{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := deck{"Ace", "Two", "Three", "Four"}
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
